2919c9aa707985b74d9c2b3d1508c81c11531e4a
Foluso Ogunsakin <folusomaine@gmail.com>
2024-04-29 02:01:03 +0100

    test vpc

---
+++ user-data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Curl DNS domains that are labeled for bitcoin
+# CryptoCurrency:EC2/BitcoinTool.B!DNS
+dig donate.v2.xmrig.com
+dig systemten.org
+dig xmr.pool.minergate.com
+dig pool.minergate.com
+dig dockerupdate.anondns.net
+dig rspca-northamptonshire.org.uk
+dig xmrpool.eu
+dig cryptofollow.com
+dig xmr-usa.dwarfpool.com
+dig xmr-eu.dwarfpool.com
+dig xmr-eu1.nanopool.org
+
+echo "DONE" > ~/done.txt
