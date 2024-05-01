ba61a54b04112b642ee6c3fb3256a2ac373037d0
Foluso Ogunsakin <folusomaine@gmail.com>
2024-05-01 12:56:22 +0100

    pass userdata as parameter

--- user-data.sh
+++ user-data.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 # Curl DNS domains that are labeled for bitcoin
 # CryptoCurrency:EC2/BitcoinTool.B!DNS
-dig donate.v2.xmrig.com
-dig systemten.org
-dig xmr.pool.minergate.com
-dig pool.minergate.com
-dig dockerupdate.anondns.net
-dig rspca-northamptonshire.org.uk
-dig xmrpool.eu
-dig cryptofollow.com
-dig xmr-usa.dwarfpool.com
-dig xmr-eu.dwarfpool.com
-dig xmr-eu1.nanopool.org
+# dig donate.v2.xmrig.com
+# dig systemten.org
+# dig xmr.pool.minergate.com
+# dig pool.minergate.com
+# dig dockerupdate.anondns.net
+# dig rspca-northamptonshire.org.uk
+# dig xmrpool.eu
+# dig cryptofollow.com
+# dig xmr-usa.dwarfpool.com
+# dig xmr-eu.dwarfpool.com
+# dig xmr-eu1.nanopool.org
 
-echo "DONE" > ~/done.txt
+cd /tmp
+dig google.com > /tmp/dig_output.txt
