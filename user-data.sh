8c39363b3aba605428ad542a6631c7603932df1f
Foluso Ogunsakin <folusomaine@gmail.com>
2024-05-02 10:07:55 +0100

    enable guardduty findings with domains with known threats

--- user-data.sh
+++ user-data.sh
@@ -2,17 +2,17 @@
 # Curl DNS domains that are labeled for bitcoin
 
 # CryptoCurrency:EC2/BitcoinTool.B!DNS
-# dig donate.v2.xmrig.com
-# dig systemten.org
-# dig xmr.pool.minergate.com
-# dig pool.minergate.com
-# dig dockerupdate.anondns.net
-# dig rspca-northamptonshire.org.uk
-# dig xmrpool.eu
-# dig cryptofollow.com
-# dig xmr-usa.dwarfpool.com
-# dig xmr-eu.dwarfpool.com
-# dig xmr-eu1.nanopool.org
+dig donate.v2.xmrig.com
+dig systemten.org
+dig xmr.pool.minergate.com
+dig pool.minergate.com
+dig dockerupdate.anondns.net
+dig rspca-northamptonshire.org.uk
+dig xmrpool.eu
+dig cryptofollow.com
+dig xmr-usa.dwarfpool.com
+dig xmr-eu.dwarfpool.com
+dig xmr-eu1.nanopool.org
 
 yum update -y
 yum install httpd -y
