415c208d03b3c5aec0ec5166824c566a1494cc4a
Foluso Ogunsakin <folusomaine@gmail.com>
2024-05-07 21:26:13 +0100

    format code

--- src/quarantine.py
+++ src/quarantine.py
@@ -13,13 +13,10 @@ def lambda_handler(event, context):
 class QuarantineHandler:
     def __init__(self, event) -> None:
         self.instance_id = event["detail"]["resource"]["instanceDetails"]["instanceId"]
-        self.vpc_id = event["detail"]["resource"]["instanceDetails"][
-            "networkInterfaces"
-        ][0]["vpcId"]
-        self.subnet_id = event["detail"]["resource"]["instanceDetails"][
-            "networkInterfaces"
-        ][0]["subnetId"]
+        self.vpc_id = event["detail"]["resource"]["instanceDetails"]["networkInterfaces"][0]["vpcId"]
+        self.subnet_id = event["detail"]["resource"]["instanceDetails"]["networkInterfaces"][0]["subnetId"]
         self.region = event["region"]
+
         self.ec2_client = boto3.client("ec2", region_name=self.region)
 
     @property
@@ -39,7 +36,7 @@ class QuarantineHandler:
         )
         return response["NetworkAcls"][0]["Associations"][0]["NetworkAclAssociationId"]
 
-    def _if_quarantine_nacl_association_exists(self):
+    def _quarantine_nacl_association_exists(self):
         """
         Check if the quarantine nacl exists
         """
@@ -55,10 +52,10 @@ class QuarantineHandler:
                 ]
             )
             if response["NetworkAcls"][0]["Associations"][0]["SubnetId"] == self.subnet_id:
-                logger.info("Quarantine nacl already enforced.")
+                logger.info("Quarantine nacl already enforced - End execution.")
                 return True
-        except (KeyError, IndexError) as e:
-            logger.info(f"Quarantine nacl does not exist. Proceed with VPC subnet Quarantine.")
+        except (KeyError, IndexError):
+            logger.info(f"Quarantine nacl does not exist - proceed with VPC subnet Quarantine.")
             return False
 
     def _create_quarantine_nacl(self):
@@ -71,7 +68,10 @@ class QuarantineHandler:
                 {
                     "ResourceType": "network-acl",
                     "Tags": [
-                        {"Key": "Name", "Value": "quarantine-nacl"},
+                        {
+                            "Key": "Name",
+                            "Value": "quarantine-nacl"
+                        },
                     ],
                 },
             ],
@@ -88,9 +88,7 @@ class QuarantineHandler:
             AssociationId=self.current_subnet_nacl_association_id,
             NetworkAclId=self.quarantine_nacl_id,
         )
-        logger.info(
-            f"Quarantine nacl associated with subnet successfully. Response: {response}"
-        )
+        logger.info(f"Quarantine nacl associated with subnet successfully. Response: {response}")
         return
 
     def quarantine_vpc(self):
@@ -98,9 +96,10 @@ class QuarantineHandler:
         Quarantine the vpc
         """
         logger.info(
-            f"Indication of compromise - instance: {self.instance_id} in {self.subnet_id}. Quarantining vpc: {self.vpc_id}"
+            f"Indication of compromise - instance: {self.instance_id} in {self.subnet_id}. " +
+            f"Quarantining vpc: {self.vpc_id}"
         )
-        if self._if_quarantine_nacl_association_exists():
+        if self._quarantine_nacl_association_exists():
             return
         self._create_quarantine_nacl()
         self._associate_quarantine_nacl_with_subnet()
